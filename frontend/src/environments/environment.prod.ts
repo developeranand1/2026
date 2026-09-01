6b865a39cbbaa47e7bb04f74bd488d4dce9c7473
Anand Prakash Chaudhary <apchaudhary8126@gmail.com>
2026-09-01 23:03:50 +0530

    mandi rate

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  // apiUrl: 'https://api.krisimarg.com/api',
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://api.krisimarg.com/api',
+  // apiUrl: 'http://localhost:5000/api',
 };
